0aeeb89a26eb96003fadc92330901a9552ae76b3
Justin Frankel <justin+mbp@cockos.com>
2021-02-15 15:32:21 -0500

    update URIs for instance capitalization consistency

--- lv2_embed_ui.h
+++ lv2_embed_ui.h
@@ -29,15 +29,15 @@ typedef struct _LV2_EMBED_UI_DrawState {
 } LV2_EMBED_UI_DrawState;
 
 // passed LV2_EMBED_UI_DrawState
-#define LV2_EMBED_UI__Draw     LV2_EMBED_UI_PREFIX "Draw"   ///< https://cockos.com/lv2/embed_ui#Draw
+#define LV2_EMBED_UI__Draw     LV2_EMBED_UI_PREFIX "draw"   ///< https://cockos.com/lv2/embed_ui#draw
 
 // passed LV2_EMBED_UI_MouseState
-#define LV2_EMBED_UI__SetCursor     LV2_EMBED_UI_PREFIX "SetCursor"   ///< https://cockos.com/lv2/embed_ui#SetCursor
-#define LV2_EMBED_UI__MouseMove     LV2_EMBED_UI_PREFIX "MouseMove"   ///< https://cockos.com/lv2/embed_ui#MouseMove
-#define LV2_EMBED_UI__MouseDown     LV2_EMBED_UI_PREFIX "MouseDown"   ///< https://cockos.com/lv2/embed_ui#MouseDown
-#define LV2_EMBED_UI__MouseUp       LV2_EMBED_UI_PREFIX "MouseUp"     ///< https://cockos.com/lv2/embed_ui#MouseUp
-#define LV2_EMBED_UI__Mouse2Down    LV2_EMBED_UI_PREFIX "Mouse2Down"  ///< https://cockos.com/lv2/embed_ui#Mouse2Down
-#define LV2_EMBED_UI__Mouse2Up      LV2_EMBED_UI_PREFIX "Mouse2Up"    ///< https://cockos.com/lv2/embed_ui#Mouse2Up
+#define LV2_EMBED_UI__setCursor     LV2_EMBED_UI_PREFIX "setCursor"   ///< https://cockos.com/lv2/embed_ui#setCursor
+#define LV2_EMBED_UI__mouseMove     LV2_EMBED_UI_PREFIX "mouseMove"   ///< https://cockos.com/lv2/embed_ui#mouseMove
+#define LV2_EMBED_UI__mouseDown     LV2_EMBED_UI_PREFIX "mouseDown"   ///< https://cockos.com/lv2/embed_ui#mouseDown
+#define LV2_EMBED_UI__mouseUp       LV2_EMBED_UI_PREFIX "mouseUp"     ///< https://cockos.com/lv2/embed_ui#mouseUp
+#define LV2_EMBED_UI__mouse2Down    LV2_EMBED_UI_PREFIX "mouse2Down"  ///< https://cockos.com/lv2/embed_ui#mouse2Down
+#define LV2_EMBED_UI__mouse2Up      LV2_EMBED_UI_PREFIX "mouse2Up"    ///< https://cockos.com/lv2/embed_ui#mouse2Up
 
 typedef struct _LV2_EMBED_UI_SizeHints {
   uint32_t preferred_aspect; // 16.16 fixed point (65536 = 1:1, 32768 = 1:2, etc)
@@ -48,7 +48,7 @@ typedef struct _LV2_EMBED_UI_SizeHints {
 } LV2_EMBED_UI_SizeHints;
 
 // passed LV2_EMBED_UI_SizeHints
-#define LV2_EMBED_UI__GetSizeHints  LV2_EMBED_UI_PREFIX "GetSizeHints"    ///< https://cockos.com/lv2/embed_ui#GetSizeHints
+#define LV2_EMBED_UI__getSizeHints  LV2_EMBED_UI_PREFIX "getSizeHints"    ///< https://cockos.com/lv2/embed_ui#getSizeHints
 
 // returned as a LV2UI_Widget
 typedef struct _LV2_EMBED_UI_Widget {
