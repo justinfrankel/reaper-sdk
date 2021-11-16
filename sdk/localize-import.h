ddac4963bf9e2122f5d69092a50b68ef9f0f2b36
Justin Frankel <justin+mbp@cockos.com>
2021-11-16 15:59:12 -0500

    add reaper-specific localize-import.h

---
+++ sdk/localize-import.h
@@ -0,0 +1,31 @@
+// used by plug-ins to access imported localization
+// usage: 
+// #define LOCALIZE_IMPORT_PREFIX "midi_" (should be the directory name with _)
+// #include "../localize-import.h"
+// ...
+// IMPORT_LOCALIZE_VST(hostcb) 
+// ...or
+// IMPORT_LOCALIZE_RPLUG(rec)
+//
+#ifdef _REAPER_LOCALIZE_H_
+#error you must include localize-import.h before localize.h, sorry
+#endif
+
+#include "../WDL/localize/localize-import.h"
+
+#ifndef _REAPER_LOCALIZE_IMPORT_H_
+#define _REAPER_LOCALIZE_IMPORT_H_
+
+#define IMPORT_LOCALIZE_VST(hostcb) \
+    *(VstIntPtr *)&importedLocalizeFunc = hostcb(NULL,0xdeadbeef,0xdeadf00d,0,(void*)"__localizeFunc",0.0); \
+    *(VstIntPtr *)&importedLocalizeMenu = hostcb(NULL,0xdeadbeef,0xdeadf00d,0,(void*)"__localizeMenu",0.0); \
+    *(VstIntPtr *)&importedLocalizeInitializeDialog = hostcb(NULL,0xdeadbeef,0xdeadf00d,0,(void*)"__localizeInitializeDialog",0.0); \
+    *(VstIntPtr *)&importedLocalizePrepareDialog = hostcb(NULL,0xdeadbeef,0xdeadf00d,0,(void*)"__localizePrepareDialog",0.0);
+
+#define IMPORT_LOCALIZE_RPLUG(rec) \
+  *(void **)&importedLocalizeFunc = rec->GetFunc("__localizeFunc"); \
+  *(void **)&importedLocalizeMenu = rec->GetFunc("__localizeMenu"); \
+  *(void **)&importedLocalizeInitializeDialog = rec->GetFunc("__localizeInitializeDialog"); \
+  *(void **)&importedLocalizePrepareDialog = rec->GetFunc("__localizePrepareDialog");
+
+#endif
